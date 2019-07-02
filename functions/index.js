6d8d58621631be4ce69e42fba04341070929aaa8
Yuta Saito <ysaito1@gmail.com>
2019-07-02 18:42:30 +0900

    lint処理

--- functions/index.js
+++ functions/index.js
@@ -7,5 +7,5 @@ const Jsdiff = require('diff')
 exports.diff = functions.https.onRequest((request, response) => {
   const text1 = request.body.text1
   const text2 = request.body.text2
-  return (response.status(200).send(Jsdiff.diffChars(text1, text2)))
+  return response.status(200).send(Jsdiff.diffChars(text1, text2))
 })

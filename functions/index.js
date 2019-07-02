fa3c6e993f9fbb91b4b168c51b32d0c6ec838e0b
Yuta Saito <ysaito1@gmail.com>
2019-07-02 18:39:05 +0900

    ；を削除

--- functions/index.js
+++ functions/index.js
@@ -5,7 +5,7 @@ const Jsdiff = require('diff')
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
 exports.diff = functions.https.onRequest((request, response) => {
-  const text1 = request.body.text1;
-  const text2 = request.body.text2;
+  const text1 = request.body.text1
+  const text2 = request.body.text2
   return (response.status(200).send(Jsdiff.diffChars(text1, text2)))
-});
+})

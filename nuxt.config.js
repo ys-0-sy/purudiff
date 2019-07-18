3ce50501bfba859b7f37abfb4db3019eee5f0cb4
Yuta Saito <saito@ssn.elec.keio.ac.jp>
2019-07-18 19:19:57 +0900

    autolintの設定を追加　

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,7 +8,8 @@ module.exports = {
   head: {
     titleTemplate: '%s - ' + process.env.npm_package_name,
     title: process.env.npm_package_name || '',
-    meta: [{
+    meta: [
+      {
         charset: 'utf-8'
       },
       {
@@ -21,14 +22,16 @@ module.exports = {
         content: process.env.npm_package_description || ''
       }
     ],
-    link: [{
+    link: [
+      {
         rel: 'icon',
         type: 'image/x-icon',
         href: '/favicon.ico'
       },
       {
         rel: 'stylesheet',
-        href: 'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons'
+        href:
+          'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons'
       }
     ]
   },

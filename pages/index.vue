d786355b5c648358b0dd13c39a109ca30d464fd0
Yuta Saito <saito@ssn.elec.keio.ac.jp>
2019-07-28 00:57:21 +0900

    Debugの削除，震える機能の完成

--- pages/index.vue
+++ pages/index.vue
@@ -1,61 +1,47 @@
 <template>
   <v-container fluid>
     <v-layout row>
-      <v-flex xs6 order-lg2>
+      <v-flex
+        xs6
+        order-lg2>
         <v-textarea
-          v-model="textbox1"
+          v-model="textBox1"
           name="textbox1"
           label="text1"
-          auto-grow
-          @input="diffText"
-        />
+          auto-grow />
       </v-flex>
-      <v-flex xs6 order-lg2>
+      <v-flex
+        xs6
+        order-lg2>
         <v-textarea
-          v-model="textbox2"
+          v-model="textBox2"
           name="textbox2"
           label="text2"
-          auto-grow
-          @input="diffText"
-        />
+          auto-grow />
       </v-flex>
     </v-layout>
     <v-layout row>
       <v-container>
         <span
-          v-for="item in difftext1"
+          v-for="item in diffText1"
           :key="item.value"
           :class="{
-            'red lighten-2': item.removed
-          }"
-        >
+            'red lighten-2 shake shake-constant': item.removed
+          }">
           {{ item.value }}
         </span>
       </v-container>
       <v-container>
         <span
-          v-for="item in difftext2"
+          v-for="item in diffText2"
           :key="item.value"
           :class="[
             {
-              'green lighten-2': item.added
-            },
-            'shake',
-            'shake-constant'
-          ]"
-        >
+              'green lighten-2 shake shake-constant': item.added
+            }
+          ]">
           {{ item.value }}
         </span>
-      </v-container>
-    </v-layout>
-    <v-layout row>
-      <v-container>
-        <v-flex xs6 order-lg2>
-          <span>{{ difftext1 }}</span>
-        </v-flex>
-      </v-container>
-      <v-container>
-        <span>{{ difftext2 }}</span>
       </v-container>
     </v-layout>
   </v-container>
@@ -67,21 +53,19 @@ import * as Jsdiff from 'diff'
 export default {
   data() {
     return {
-      textbox1: 'sample text1',
-      textbox2: 'sample text2',
-      difftext1: '',
-      difftext2: ''
+      textBox1: 'sample text1',
+      textBox2: 'sample text2'
     }
   },
-  methods: {
-    diffText() {
-      return this.parseTexts(Jsdiff.diffChars(this.textbox1, this.textbox2))
-    },
-    parseTexts(difftext) {
-      this.difftext1 = difftext.filter((words) => {
+  computed: {
+    // eslint-disable-next-line prettier/prettier
+    diffText1: function () {
+      return Jsdiff.diffChars(this.textBox1, this.textBox2).filter((words) => {
         return !words.added
       })
-      this.difftext2 = difftext.filter((words) => {
+    },
+    diffText2: function () {
+      return Jsdiff.diffChars(this.textBox1, this.textBox2).filter((words) => {
         return !words.removed
       })
     }
@@ -91,26 +75,4 @@ export default {
 
 <style lang="scss">
 @import 'csshake';
-.buruburu {
-  display: inline-block;
-  animation: hurueru 0.1s infinite;
-}
-
-@keyframes hurueru {
-  0% {
-    transform: translate(0px, 0px) rotateZ(0deg);
-  }
-  25% {
-    transform: translate(2px, 2px) rotateZ(1deg);
-  }
-  50% {
-    transform: translate(0px, 2px) rotateZ(0deg);
-  }
-  75% {
-    transform: translate(2px, 0px) rotateZ(-1deg);
-  }
-  100% {
-    transform: translate(0px, 0px) rotateZ(0deg);
-  }
-}
 </style>

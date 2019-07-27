a2898214737b5327abd79eed409544953b529ce9
Yuta Saito <saito@ssn.elec.keio.ac.jp>
2019-07-28 01:22:38 +0900

    pcakageの調整

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,43 +1,24 @@
 module.exports = {
-  plugins: [
-    "vue"
-  ],
+  root: true,
+  env: {
+    browser: true,
+    node: true
+  },
+  parserOptions: {
+    parser: 'babel-eslint'
+  },
   extends: [
-    'eslint:recommended',
-    'plugin:vue/recommended'
+    '@nuxtjs',
+    'plugin:nuxt/recommended'
   ],
+  plugins: [
+    'prettier'
+  ],
+  // add your custom rules here
   rules: {
-    // タグの最後で改行しないで
-    "vue/html-closing-bracket-newline": [2, {"multiline": "never"}],
-    // 不要なカッコは消す
-    "no-extra-parens": 1,
-    // 無駄なスペースは削除
-    "no-multi-spaces": 2,
-    // 不要な空白行は削除。2行開けてたらエラー
-    "no-multiple-empty-lines": [2, {"max": 1}],
-    // 関数とカッコはあけない(function hoge() {/** */})
-    "func-call-spacing": [2, "never"],
-    // true/falseを無駄に使うな
-    "no-unneeded-ternary": 2,
-    // セミコロンは禁止
-    "semi": [2, "never"],
-    // 文字列はシングルクオートのみ
-    "quotes": [2, "single"],
-    // varは禁止
-    "no-var": 2,
-    // jsのインデントは２
-    "indent": [2, 2],
-    // かっこの中はスペースなし！違和感
-    "space-in-parens": [2, "never"],
-    // コンソールは許可
+    'nuxt/no-cjs-in-config': 'off',
     "no-console": 0,
-    // カンマの前後にスペース入れる？
-    "comma-spacing": 2,
-    // 配列のindexには空白入れるな(hogehoge[ x ])
-    "computed-property-spacing": 2,
-    // キー
-    "key-spacing": 2,
-    // キーワードの前後には適切なスペースを
-    "keyword-spacing": 2,
+    "no-unused-vars": 1,
+    "vue/no-unused-components": 1
   }
 }
